8818006e0293e8921dbf0583c7305c23648dc6cc
Windows10 <Windows10@DESKTOP-73KSJ4T>
2025-07-22 03:52:51 +0300

    Add project files.

---
+++ PratikTryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Ask the user to enter a number
+            // Kullanıcıdan bir sayı girmesini istiyoruz
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate and print the square of the number
+            // Sayının karesini hesaplayıp ekrana yazdırıyoruz
+            int square = number * number;
+            Console.WriteLine("The square of the number is: " + square);
+        }
+        catch (FormatException)
+        {
+            // If the user enters a non-numeric value, show error message
+            // Kullanıcı geçersiz (harf/simge) bir giriş yaparsa, hata mesajı gösteriyoruz
+            Console.WriteLine("Invalid input! Please enter a number.");
+        }
+    }
+}
+
